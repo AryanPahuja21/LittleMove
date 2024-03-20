ba15d078cbfe45021ba5ff35235e047f28ae10b7
AryanPahuja21 <aryanpahuja1421@gmail.com>
2024-03-20 18:41:20 +0530

    navbar added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { AiOutlineClose } from "react-icons/ai";
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const navLinks = [
+    { href: "#home", label: "Home" },
+    { href: "#about-us", label: "About Us" },
+    { href: "#contact-us", label: "Contact Us" },
+  ];
+  return (
+    <>
+      <header className="sm:px-8 px-4 py-5 z-10 w-full bg-primary rounded-b-3xl">
+        <nav className="flex justify-between items-center max-container">
+          <a href="/" className="text-3xl font-bold">
+            LittleMove
+          </a>
+          <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+            {navLinks.map((item) => (
+              <li key={item.label}>
+                <a
+                  href={item.href}
+                  className="font-montserrat leading-normal text-lg text-slate-gray font-medium"
+                >
+                  {item.label}
+                </a>
+              </li>
+            ))}
+          </ul>
+          <div className="flex gap-2 text-lg leading-normal font-semibold font-montserrat max-lg:hidden wide:mr-24">
+            <a href="/">Sign in</a>
+            <span>/</span>
+            <a href="/">Explore now</a>
+          </div>
+          <div
+            className="hidden max-lg:block cursor-pointer"
+            onClick={() => {
+              setIsMenuOpen(!isMenuOpen);
+            }}
+          >
+            <RxHamburgerMenu className="text-4xl" />
+          </div>
+        </nav>
+      </header>
+      {isMenuOpen && (
+        <div>
+          <nav className="fixed top-0 right-0 left-0 bottom-0 lg:bottom-auto bg-primary">
+            <div
+              className="hidden max-lg:block fixed right-0  px-8 py-4 cursor-pointer"
+              onClick={() => {
+                setIsMenuOpen(!isMenuOpen);
+              }}
+            >
+              <AiOutlineClose className="text-4xl" />
+            </div>
+            <ul className=" lg:hidden flex flex-col items-center justify-center h-full ">
+              {navLinks.map((item) => (
+                <li key={item.label}>
+                  <a
+                    href={item.href}
+                    className="font-montserrat leading-loose text-2xl text-slate-gray font-semibold"
+                  >
+                    {item.label}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </div>
+      )}
+    </>
+  );
+};
+export default Navbar;
